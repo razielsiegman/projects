c10ef7ece6c33529e580ea13948e3d5af10a6f3a
razielsiegman <rsiegman@mail.yu.edu>
2019-09-01 22:30:11 +0300

    myfirstcheckin

---
+++ introtocompsci/assignments/HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello YC CS World!");
+        System.out.println("Linenumbertwo");
+    }
+}
